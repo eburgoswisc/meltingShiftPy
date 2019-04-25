784f82bcdfc236bcaf05125a385c19736af95a56
Emanuel Burgos <eburgos@wisc.edu>
2019-04-25 10:09:52 -0500

    Clean up

--- .ipynb_checkpoints/utils-checkpoint.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import pandas as pd
-
-def tm_calculate(s: pd.Series) -> float:
-    return (s["maxTemp"])/ ( 2 * (s["maxTemp"] - s["minTemp"]) )
-
-def melting_shift(sample:pd.Series, control=pd.Series):
-    return (sample["Melting Temperature"] - control["Melting Temperature"]) >= 1
