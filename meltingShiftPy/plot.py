191961416f5b9b13005f93c64fa8d2c766d93300
Emanuel Burgos <eburgos@wisc.edu>
2019-05-16 19:24:16 -0500

    Fix grid plot rendering issue, use command --grid.

--- meltingShiftPy/plot.py
+++ meltingShiftPy/plot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import pandas as pd
-import matplotlib.pyplot as plt
 import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
 
 # TODO: fix plotting style
 # Update matplotlib rcParameters
@@ -25,7 +26,7 @@ def plot_qs7(df, control, axis_spec, color="red"):
     fig = plt.figure(constrained_layout=True)
     fig.set_size_inches(25,25)
     # Create gridplot
-    gs = plt.GridSpec(axis_spec, axis_spec, figure = fig)
+    gs = plt.GridSpec(axis_spec, axis_spec, figure=fig)
     # Loop for plotting
     i = 0
     while i < axis_spec:
@@ -50,11 +51,59 @@ def plot_qs7(df, control, axis_spec, color="red"):
             
         # Increment placeholder
         i += 1
-        
-    # Open figure window    
+
+    # Open figure window
     plt.show()
 
-def plot_cfx(df, control, axis_spec, color="red"):
+
+def plot_cfx(df, control, output, color="red"):
+    """
+    Plotting function for results from cfx thermocycler
+
+    :param df:
+    :param control:
+    :param axis_spec:
+    :param color:
+    :return:
+    """
+
+    #plt.ion()
+    # Create custom figure
+    #fig = plt.figure(constrained_layout=True)
+    #fig.set_size_inches(36,36)
+    # Create gridplot
+    #gs = plt.GridSpec(axis_spec, axis_spec, figure=fig)
+    # Create iterator of columns for dataframe
+    column_iterator = df.iteritems()
+    # Loop for plotting
+    i = 1
+    while i < len(df.columns):
+        print(f"Loop {i}")
+        fig = plt.figure(constrained_layout=True)
+        fig.set_size_inches(40, 40)
+        gs = plt.GridSpec(4,4, figure=fig)
+        for j in range(4):
+            for l in range(4):
+                try:
+                    # Call next sample replicate
+                    sample = next(column_iterator)
+                except:
+                    plt.show()
+                # Create plot
+                ax = plt.subplot(gs[l, j], label=sample[1].name)
+                # Plotting sample vs control
+                ax.set(title=sample[1].name, xlabel="Temperature", ylabel="Flourescence", xlim=(min(sample[1].index), 70))
+                ax.yaxis.set_major_formatter(mpl.ticker.EngFormatter())
+                # Plot control vs sample
+                ax.scatter(x=control.index, y=sample[1], color=color)
+                ax.scatter(x=control.index, y=control)
+
+        # Increment placeholder
+        i += 12
+    print("Done")
+
+
+def plot_cfx_onegrid(df, control, axis_spec, output, color="red"):
     """
     Plotting function for results from cfx thermocycler
 
@@ -65,9 +114,10 @@ def plot_cfx(df, control, axis_spec, color="red"):
     :return:
     """
     # Create custom figure
-    fig = plt.figure(constrained_layout=False)
-    fig.set_size_inches(36,36)
+    fig = plt.figure(constrained_layout=True)
+    # fig.set_size_inches(36,36)
     # Create gridplot
+    print(axis_spec)
     gs = plt.GridSpec(axis_spec, axis_spec, figure=fig)
     # Create iterator of columns for dataframe
     column_iterator = df.iteritems()
@@ -81,19 +131,16 @@ def plot_cfx(df, control, axis_spec, color="red"):
             except:
                 i = axis_spec
                 break
-
+            print("here")
             # Create plot
             ax = plt.subplot(gs[j, i])
             # Plotting sample vs control
             ax.set(title=sample[1].name, xlabel="Temperature", ylabel="Flourescence", xlim=(min(sample[1].index), 70))
             ax.yaxis.set_major_formatter(mpl.ticker.EngFormatter())
             # Plot control vs sample
-
             ax.scatter(x=control.index, y=sample[1], color=color)
             ax.scatter(x=control.index, y=control)
-
+        plt.show()
         # Increment placeholder
         i += 1
-
-    # Open figure window
-    plt.show()
+    print("Done")
